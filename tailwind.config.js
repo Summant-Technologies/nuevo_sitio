34bb550c8df457174c059548ea0e3962eb092964
Agus Pereyra <agustinpereyra@summant.com>
2024-02-07 11:41:57 -0300

    Set variables from primary and secondary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}","./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#1723FF',
+        'secondary': '#30E35E',
+      },
+    },
   },
   plugins: [],
 }
