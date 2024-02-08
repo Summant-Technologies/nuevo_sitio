f4f92aead47f48848d52911585484339de037c40
Agus Pereyra Summant <157617991+AgusPereyra13@users.noreply.github.com>
2024-02-08 16:44:44 -0300

    Agus (#14)
    * Add Cegid Section with the tittles
    
    * Cegid section finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,18 @@ module.exports = {
       colors: {
         'primary': '#1723FF',
         'secondary': '#30E35E',
+        'info': '#717171',
       },
       fontFamily: {
         'inter': ['Inter', 'sans-serif'],
       },
       dropShadow: {
         'services': '2px 2px 5px rgb(0, 0, 0, 0.1)'
-      }
+      },
+      backgroundImage: {
+        'hexagon-right': "url('./images/PARTNERS/PNG/Right.png')",
+        'hexagon-left': "url('./images/PARTNERS/PNG/Left.png')"
+      },
     },
   },
   plugins: [],
