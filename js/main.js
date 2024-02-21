af30d4debc07f5ca03bc03dd57a180a65b172147
LautaroCastilloSmm <157618817+Laucastillo25@users.noreply.github.com>
2024-02-21 12:30:09 -0300

    Contactanos footer (#49)
    * Add contactanos and footer section TODO:images
    
    * Add the images and fixed the padding of the contactanos
    
    * Remove innecesary code in js

--- js/main.js
+++ js/main.js
@@ -9,20 +9,6 @@
 //     loop: true
 //   });
 
-// Este es el codigo para el slider de las resenas  
-const prev = document.getElementById('prev-btn')
-const next = document.getElementById('next-btn')
-const list = document.getElementById('item-list')
-const itemWidth = 150
-const padding = 10
-
-prev.addEventListener('click', () => {
-    list.scrollLeft -= (itemWidth + padding)
-})
-next.addEventListener('click', () => {
-    list.scrollLeft += (itemWidth + padding)
-})
-
 
 // Este es el codigo para el slider de los clientes
 
