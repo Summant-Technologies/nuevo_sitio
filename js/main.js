23d030c30bb8254d3c86c9b7151d5008dd0bd325
Lautaro Castillo <lautarocastillo@summant.com>
2024-02-23 15:23:17 -0300

    Add animation with js to the card vision in porque summant

--- js/main.js
+++ js/main.js
@@ -9,6 +9,75 @@
 //     loop: true
 //   });
 
+// Inicio codigo de las cards de la section porque
+
+const cardVision = document.querySelector(".card--vision");
+const cardMision = document.querySelector(".card--mision");
+const cardValores = document.querySelector(".card--valores");
+
+const btnVision = document.querySelector("#btn-vision");
+const btnMision = document.querySelector("#btn-mision");
+const btnValores = document.querySelector("#btn-valores");
+
+
+function visionCard() {
+    let imgBg = "images/EXPORTS/PORQUE/businesspeople-celebrating-victory 1.png";
+    let icon = "images/ICONOS/PORQUE/BANDERIN.svg";
+    let text = "Vision";
+
+    const content = document.createElement("p");
+    content.textContent = `Ser una empresa de IT valorada por nuestros clientes, construyendo
+con ellos un vínculo de confianza basados en la calidad de nuestros productos y servicios y en las cualidades humanas y profesionales de nuestros colaboradores`
+
+    changeCardContent(cardVision, imgBg, icon, text, content)
+}
+
+// Esta funcion crea el contenido de la card en porque summant cuando se hace click
+function changeCardContent(card, imgBgUrl, iconUrl, text, content) {
+    card.textContent = "";
+    card.classList.add("card-mod");
+
+    // Add the img for the bg
+    const bgImg = document.createElement("img");
+    bgImg.src = imgBgUrl;
+    bgImg.classList.add("card-bg");
+
+    card.appendChild(bgImg);
+
+    // Create the div from the top
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("card__title");
+
+    const titleImg = document.createElement("img");
+    titleImg.src = iconUrl;
+    titleImg.classList.add("card__title-img")
+
+    const titleText = document.createElement("p");
+    titleText.textContent = text;
+    titleText.classList.add("card__title-text")
+
+    titleDiv.appendChild(titleImg);
+    titleDiv.appendChild(titleText);
+
+    card.appendChild(titleDiv);
+
+    // Create the separator of the card
+    const separator = document.createElement("div");
+    separator.classList.add("card-separator");
+
+    card.appendChild(separator);
+
+    //Create the content of the card
+
+    content.classList.add("card-content");
+
+
+    card.appendChild(content);
+}
+
+btnVision.addEventListener("click", visionCard);
+
+// Fin cards section porque
 
 // Este es el codigo para el slider de los clientes
 
