fcbf414e3e165413062537e9a4421ac5e8ba50d7
LautaroCastilloSmm <157618817+Laucastillo25@users.noreply.github.com>
2024-02-28 17:59:06 -0300

    Styles padding (#62)
    * Fix styles and margin issues
    
    * Fix errors in tech section
    
    * Fix styling in resenas slider and clientes section
    
    * Fix some styling in sliders and porque section

--- js/main.js
+++ js/main.js
@@ -209,6 +209,9 @@ const glideResenas = new Glide("#glide-resenas", {
     768: {
       perView: 1
     },
+    1200: {
+      focusAt: 1
+    }
   }
 });
 
@@ -216,17 +219,18 @@ glideResenas.mount();
 
 const glideTech = new Glide("#glide-tech", {
   type: 'carousel',
-  perView: 4,
-  autoplay: 3000,
-  hoverpause: true,
+  perView: 5,
+  autoplay: 4000,
   gap: 80,
   focusAt: 1,
   breakpoints: {
-    1200: {
-      perView: 3
-    },
     768: {
-      perView: 2
+      perView: 3,
+      gap: 10
+    },
+    460: {
+      perView: 2,
+      gap: 20,
     },
   }
 });
