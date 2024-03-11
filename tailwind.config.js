6927633174a610f0043f39365103caed7a25b5b8
APereyra-SM <157617991+AgusPereyra13@users.noreply.github.com>
2024-03-11 12:56:29 -0300

    Add quality section (#66)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","./index.html"],
+  content: ["./src/**/*.{html,js}","./index.html","./politica-calidad.html"],
   theme: {
     extend: {
       colors: {
